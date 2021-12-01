9b7d93a44e4b7a4439c9b6e6e8096c5a9c88dc69
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-01 22:41:46 +0300

    project template created

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).res.send("index page");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
